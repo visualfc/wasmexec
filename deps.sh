1e44811b57849a07e540f96c50890c31bfa1c154
visualfc <visualfc@gmail.com>
2023-09-06 21:18:54 +0800

    fix deps.sh

--- deps.sh
+++ deps.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-curl -o wasm/wasm_exec_go116.js https://github.com/golang/go/blob/go1.16.15/misc/wasm/wasm_exec.js
-curl -o wasm/wasm_exec_go117.js https://github.com/golang/go/blob/go1.17.13/misc/wasm/wasm_exec.js
-curl -o wasm/wasm_exec_go118.js https://github.com/golang/go/blob/go1.18.10/misc/wasm/wasm_exec.js
-curl -o wasm/wasm_exec_go119.js https://github.com/golang/go/blob/go1.19.12/misc/wasm/wasm_exec.js
-curl -o wasm/wasm_exec_go120.js https://github.com/golang/go/blob/go1.20.7/misc/wasm/wasm_exec.js
-curl -o wasm/wasm_exec_go121.js https://github.com/golang/go/blob/go1.21.0/misc/wasm/wasm_exec.js
+curl -o wasm/wasm_exec_go116.js https://raw.githubusercontent.com/golang/go/go1.16.15/misc/wasm/wasm_exec.js
+curl -o wasm/wasm_exec_go117.js https://raw.githubusercontent.com/golang/go/go1.17.13/misc/wasm/wasm_exec.js
+curl -o wasm/wasm_exec_go118.js https://raw.githubusercontent.com/golang/go/go1.18.10/misc/wasm/wasm_exec.js
+curl -o wasm/wasm_exec_go119.js https://raw.githubusercontent.com/golang/go/go1.19.12/misc/wasm/wasm_exec.js
+curl -o wasm/wasm_exec_go120.js https://raw.githubusercontent.com/golang/go/go1.20.7/misc/wasm/wasm_exec.js
+curl -o wasm/wasm_exec_go121.js https://raw.githubusercontent.com/golang/go/go1.21.0/misc/wasm/wasm_exec.js
